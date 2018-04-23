ae9e03d311b613a8f99711756b9918eb2d284451
Justin Bloom <bloomju@oregonstate.edu>
2018-04-22 21:14:21 -0700

    Wrote a bunch of stuff that looks about right but doesn't work

---
+++ hw1.hs
@@ -0,0 +1,21 @@
+module hw1 where
+
+-- Exercise 1
+
+
+
+data cmd =  pen mode
+        |   moveto (pos, pos)
+        |   def String pars cmd
+        |   call String vals
+        |   multiCMD cmd cmd
+
+data mode = up | down
+data pos = num Int | name String
+data pars = multiPars String pars | singlePar String
+data vals = multiVals Int vals | singleVal Int
+
+
+
+
+
