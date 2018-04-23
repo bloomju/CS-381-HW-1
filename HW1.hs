1aace5183692ab39f56f5ad1b8fa33ce1a789dd4
Justin Bloom <bloomju@oregonstate.edu>
2018-04-23 16:48:10 -0700

    may or may not have finished exercise 1, it compiles and I think it's correct

--- HW1.hs
+++ HW1.hs
@@ -6,7 +6,7 @@ data Cmd =  Pen Mode
         |   MoveTo (Pos, Pos)
         |   Def String Pars Cmd
         |   Call String Vals
-        |   MultiCMD Cmd Cmd
+        |   MultiCmd Cmd Cmd
 
 data Mode = Up | Down
 data Pos = Num Int | Name String
@@ -15,7 +15,24 @@ data Vals = MultiVals Int Vals | SingleVal Int
 
 vector (x1, y1) (x2, y2) = 
                 Def "vector" (MultiPars "p1" (SinglePar "p2"))
-                (MultiCMD (Pen Up) 
-                (MultiCMD (MoveTo (Num x1, Num y1))
-                (MultiCMD (Pen Down)
+                (MultiCmd (Pen Up) 
+                (MultiCmd (MoveTo (Num x1, Num y1))
+                (MultiCmd (Pen Down)
                 (MoveTo (Num x2, Num y2)))))
+
+steps :: Int -> Cmd
+
+steps 0 = MultiCmd (Pen Up) (MoveTo (Num 0, Num 0))
+
+steps n = 
+            MultiCmd (Pen Up)
+            (MultiCmd (MoveTo (Num n, Num n))
+            (MultiCmd (MoveTo (Num (n-1), Num n))
+            (MultiCmd (MoveTo(Num (n-1), Num (n-1)))
+            (steps (n-1)) )))
+
+
+
+
+
+
