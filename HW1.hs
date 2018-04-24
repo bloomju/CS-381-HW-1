c39aafc656c0777a3d2e48198a1a777d41c79fd4
Justin Bloom <bloomju@oregonstate.edu>
2018-04-24 15:35:35 -0700

    made changes based on TA suggestions

--- HW1.hs
+++ HW1.hs
@@ -13,12 +13,14 @@ data Pos = Num Int | Name String
 data Pars = MultiPars String Pars | SinglePar String
 data Vals = MultiVals Int Vals | SingleVal Int
 
+vector :: (Pos, Pos) -> (Pos, Pos) -> Cmd
+
 vector (x1, y1) (x2, y2) = 
                 Def "vector" (MultiPars "p1" (SinglePar "p2"))
                 (MultiCmd (Pen Up) 
-                (MultiCmd (MoveTo (Num x1, Num y1))
+                (MultiCmd (MoveTo (x1, y1))
                 (MultiCmd (Pen Down)
-                (MoveTo (Num x2, Num y2)))))
+                (MoveTo (x2, y2) ))))
 
 steps :: Int -> Cmd
 
@@ -27,9 +29,10 @@ steps 0 = MultiCmd (Pen Up) (MoveTo (Num 0, Num 0))
 steps n = 
             MultiCmd (Pen Up)
             (MultiCmd (MoveTo (Num n, Num n))
+            (MultiCmd (Pen Down)
             (MultiCmd (MoveTo (Num (n-1), Num n))
             (MultiCmd (MoveTo(Num (n-1), Num (n-1)))
-            (steps (n-1)) )))
+            (steps (n-1)) ))))
 
 
 
