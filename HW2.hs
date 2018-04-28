22b15a03c739e392c7f9ff535914b4557d0c8050
Justin Bloom <bloomju@oregonstate.edu>
2018-04-27 19:45:59 -0700

    wrote working semS function

--- HW2.hs
+++ HW2.hs
@@ -1,5 +1,5 @@
 -- CS381, Spring 2018
--- Assignment 1
+-- Assignment 2
 -- Justin Bloom, David Jansen, Meghana Kolasani, Rushil Vora
 
 module HW2 where
@@ -17,6 +17,7 @@ data Cmd = LD Int
 type Stack = [Int]
 type D = Stack -> Stack
 
+{-
 semCmd :: Cmd -> D
 
 semCmd (LD x) s = x:s
@@ -30,3 +31,34 @@ semCmd (DUP x:xs) = x:x:xs
 sem :: Prog -> D
 
 sem (x:xs) s = sem xs (semCmd x s)
+-}
+
+
+
+-- Exercise 3 --
+data LogoCmd = Pen Mode
+                | MoveTo Int Int
+                | Seq LogoCmd LogoCmd
+                deriving Show
+data Mode = Up | Down deriving Show
+
+type State = (Mode, Int, Int)
+
+type Line = (Int, Int, Int, Int)
+type Lines = [Line]
+
+semS :: LogoCmd -> State -> (State,Lines)
+
+semS (Pen m) (_,x,y) = ((m,x,y), [])
+semS (MoveTo a b) (Down, x, y)  = ((Down, a, b), [(x,y,a,b)])
+semS (MoveTo a b) (Up, x, y) = ((Up, a, b), [])
+semS (Seq c1 c2) (m, x, y) = ((m'', a', b'), l1++l2) 
+    where   ((m'',a',b'), l1) = semS c1 (m',a,b) 
+            ((m',a,b), l2) = semS c2 (m,x,y)
+ 
+
+
+
+--sem' :: LogoCmd -> Lines
+
+
